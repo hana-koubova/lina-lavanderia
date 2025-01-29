79424ad712af283c37234d286ec3ad2143d29e8f
Hana Koubova <koubovahan@gmail.com>
2025-01-29 10:00:48 +0100

    env variables config

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.secret_key = 'your_secret_key_here'
 app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587  # You can also use 2525 or 25
 app.config['MAIL_USERNAME'] = 'api'
-app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
